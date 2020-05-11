ae1d9f7de475a46b385ebbad3622c7e976ccd8f6
Emad Bin Abid <emad.bin.abid@gmail.com>
2020-05-11 18:30:51 +0500

    Updated register() and login().

--- src/auth.js
+++ src/auth.js
@@ -1,17 +1,67 @@
+const firebaseConfig = require("../config/firebase-config");
+
+const admin = firebaseConfig.admin;
+const firebase = firebaseConfig.firebase;
+
 // Check authtoken
-exports.checkAuth = function(req, res, next) {
+exports.checkAuth = function (req, res, next) {
     if (req.path == '/login' || req.path == '/register') {
-      return next();
+        return next();
     }
     if (req.headers.authtoken) {
-      admin.auth().verifyIdToken(req.headers.authtoken)
-        .then(() => {
-          next()
-        }).catch(() => {
-          res.status(403).send('Unauthorized')
-        });
+        admin.auth().verifyIdToken(req.headers.authtoken)
+            .then(() => {
+                next()
+            }).catch(() => {
+                res.status(403).send('Unauthorized')
+            });
     } else {
-      res.status(403).send('Unauthorized')
+        res.status(403).send('Unauthorized')
     }
-  }
-  
+}
+
+exports.register = function (req, res) {
+    admin.auth().createUser({
+        email: req.body.email,
+        password: req.body.password,
+    })
+        .then(function (userRecord) {
+            console.log('Successfully created new user:', userRecord.uid);
+            res.json({
+                message: `Successfully created new user:, ${userRecord.uid}`
+            });
+        })
+        .catch(function (error) {
+            console.log('Error creating new user:', error);
+            res.json({
+                message: `Error creating new user:, ${error}`
+            });
+        });
+}
+
+exports.login = function (req, res) {
+    firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.password)
+        .then(function (userRecord) {
+            console.log('Successfully signed in:', userRecord.user.refreshToken);
+            firebase.auth().currentUser.getIdToken(true)
+                .then(function (idToken) {
+                    console.log(`Successfully generated token: ${idToken}`);
+                    res.json({
+                        message: "Succesfully signed in",
+                        token: idToken
+                    });
+                })
+                .catch(function (error) {
+                    console.log('Error while generating token:', error);
+                });
+            res.json({
+                message: `Successfully signed in:, ${userRecord}`
+            });
+        })
+        .catch(function (error) {
+            console.log('Error while signing in:', error);
+            res.json({
+                message: `Error while signing in:, ${error}`
+            });
+        });
+}
