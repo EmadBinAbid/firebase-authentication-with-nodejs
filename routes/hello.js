37048f39e7af0b7539d7d21cfa98537710412af9
Emad Bin Abid <emad.bin.abid@gmail.com>
2020-05-11 17:58:55 +0500

    Added dummy routes.

---
+++ routes/hello.js
@@ -0,0 +1,15 @@
+exports.sayHelloWorld = function(app) {
+    app.get('/hello-world', (req, res) => {
+        res.json({
+            message: 'Hello World!'
+        })
+    })
+}
+
+exports.sayHelloUser = function(app) {
+    app.get('/hello-user', (req, res) => {
+        res.json({
+            message: 'Hello User!'
+        })
+    })
+}
