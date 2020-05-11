3723ef9420463fc0e2065d2edacb722eb8dd7a96
Emad Bin Abid <emad.bin.abid@gmail.com>
2020-05-11 18:31:38 +0500

    Updated routes.

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,27 @@
 const authSrc = require("../src/auth");
 
-exports.register = function(app) {
-    
+exports.register = function (app, admin) {
+    app.post('/register', (req, res) => {
+        if (req.body.email && req.body.password) {
+            authSrc.register(req, res);
+        }
+        else {
+            res.json({
+                message: 'Request payload must contain email and password'
+            })
+        }
+    })
 }
 
-exports.login = function(app) {
-    
+exports.login = function (app, firebase) {
+    app.post('/login', (req, res) => {
+        if (req.body.email && req.body.password) {
+            authSrc.login(req, res);
+        }
+        else {
+            res.json({
+                message: 'Request payload must contain email and password'
+            })
+        }
+    })
 }
