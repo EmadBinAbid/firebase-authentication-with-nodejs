e2898cd513f1c415dcb10d3a9ed2fcb98ce0316b
Emad Bin Abid <emad.bin.abid@gmail.com>
2020-05-11 18:31:15 +0500

    Registered routes.

--- app.js
+++ app.js
@@ -6,25 +6,6 @@ const cors = require('cors')
 
 const authSrc = require('./src/auth');
 
-// Firebase Setup
-var admin = require("firebase-admin");
-const firebase = require("firebase");
-var serviceAccount = require("./docs/service-key.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "YOUR_DATABASE_URL"
-});
-
-firebase.initializeApp({
-  apiKey: "YOUR_API_KEY",
-  projectId: "YOUR_PROJECT_ID",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID"
-});
-
 var app = express();
 
 app.use(logger('dev'));
