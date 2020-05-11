eabf1437858c6928da8ce42b4ea972aabaf1017b
Emad Bin Abid <emad.bin.abid@gmail.com>
2020-05-11 17:28:25 +0500

    Created express server with express generator.

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const cors = require('cors')
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+module.exports = app;
